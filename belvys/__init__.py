0b538c037278ea91376d31f8129e3937ec0a4cc2
Ruud Wijtvliet <ruud.wijtvliet@lichtblick.de>
2022-08-18 18:36:59 +0200

    bugfix

--- belvys/__init__.py
+++ belvys/__init__.py
@@ -1,11 +1,9 @@
 """Getting data from Kisters' portfolio management system 'Belvis'."""
 
-from .tenant import Tenant
+from . import _version, aftercarestore
 from .api import Api
 from .structure import Structure
-
-from .user_facing import example_structure, example_structure_to_file
-
-from . import _version
+from .tenant import Tenant
+from .example import example_structure, example_structure_to_file
 
 __version__ = _version.get_versions()["version"]
