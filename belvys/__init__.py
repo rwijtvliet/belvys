ec926d7f8fd2f7a3d0b06b5fb93ee6f905835528
Ruud Wijtvliet <ruud.wijtvliet@lichtblick.de>
2022-11-30 17:28:12 +0100

    black

--- belvys/__init__.py
+++ belvys/__init__.py
@@ -1,14 +1,14 @@
 """Getting data from Kisters' portfolio management system 'Belvis'."""
 
-from . import _version, aftercare
+from . import _version, adjustment
 from .api import Api
-from .structure import Structure, TsTree, Ts
-from .tenant import Tenant
 from .example import (
-    example_structure,
-    example_structure_to_file,
     example_api,
     example_api_to_file,
+    example_structure,
+    example_structure_to_file,
 )
+from .structure import Structure, Ts, TsTree
+from .tenant import Tenant
 
 __version__ = _version.get_versions()["version"]
